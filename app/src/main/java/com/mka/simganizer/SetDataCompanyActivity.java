18092a5832f28d93b3c3d43e877ee3129bd641c5
mustafakameldev <mustafakameldev@gmail.com>
2018-07-12 17:16:22 +0200

    comment

--- app/src/main/java/com/mka/simganizer/SetDataCompanyActivity.java
+++ app/src/main/java/com/mka/simganizer/SetDataCompanyActivity.java
@@ -2,6 +2,7 @@ package com.mka.simganizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SetDataCompanyActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class SetDataCompanyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_data_company);
+        Toast.makeText(this, "hello  , we are waiting for first line of our code", Toast.LENGTH_SHORT).show();
     }
 
 
